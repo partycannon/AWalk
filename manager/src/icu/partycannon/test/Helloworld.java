316e97274af4e59f2574ef4e327a0e6855d586be
partycannon <fcwchzy@163.com>
2022-03-03 12:36:17 +0800

    v0.0.0.20220303_base 搭建整体框架，三个module，分别为server、client和manager

---
+++ manager/src/icu/partycannon/test/Helloworld.java
@@ -0,0 +1,10 @@
+package icu.partycannon.test;
+
+import org.junit.Test;
+
+public class Helloworld {
+    @Test
+    public void helloworld() {
+        System.out.println("Hello World");
+    }
+}
